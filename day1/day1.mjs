ab2cce03331704df4ab34bf5084a097061079d87
Evie Skinner <evie.skinner@madetech.com>
2023-01-06 17:38:31 +0000

    Splitting file lines at each double newline to get each Elf's snacks

--- day1/day1.mjs
+++ day1/day1.mjs
@@ -3,6 +3,6 @@ import { readFileSync } from "node:fs";
 const inputLines = readFileSync("./input.txt", { encoding: "utf-8" })
 .replace(/\r/g, "")
 .trim()
-.split("\n \n");
+.split("\n\n");
 
 console.log(inputLines);
