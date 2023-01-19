130ac130437c66513a0ab8b06c446c3dfe82d6e8
Evie Skinner <evie.skinner@madetech.com>
2023-01-19 15:35:54 +0000

    Day1 part 1

--- day1/day1.mjs
+++ day1/day1.mjs
@@ -1,8 +1,16 @@
 import { readFileSync } from "node:fs";
+import internal from "node:stream";
 
 const inputLines = readFileSync("./input.txt", { encoding: "utf-8" })
 .replace(/\r/g, "")
 .trim()
 .split("\n\n");
 
-console.log(inputLines);
+
+const totalCaloriesForEachElf = inputLines.map((caloriesForElfString) => { 
+    const eachElfsSnackage = caloriesForElfString.split("\n").map((snackString) => parseInt(snackString.trim()));
+    return eachElfsSnackage.reduce((previous, current) => previous + current, 0);
+});
+
+const mostCalorificSnackage = Math.max(...totalCaloriesForEachElf);
+console.log(mostCalorificSnackage);
