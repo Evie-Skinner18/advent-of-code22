af119e2e3ba61c4259c95a16b33114d07cb2ae2a
Evie Skinner <evie.skinner@madetech.com>
2023-01-19 15:55:31 +0000

    Day1 part 2

--- day1/day1.mjs
+++ day1/day1.mjs
@@ -9,8 +9,31 @@ const inputLines = readFileSync("./input.txt", { encoding: "utf-8" })
 
 const totalCaloriesForEachElf = inputLines.map((caloriesForElfString) => { 
     const eachElfsSnackage = caloriesForElfString.split("\n").map((snackString) => parseInt(snackString.trim()));
-    return eachElfsSnackage.reduce((previous, current) => previous + current, 0);
+    return getTotalCaloriesFromArrayOfSnacks(eachElfsSnackage);
 });
 
+// pt1 the elf with the most calories available
 const mostCalorificSnackage = Math.max(...totalCaloriesForEachElf);
+
+// pt2 the top and bottom three snack hoarders
+
+function getBottomThreeSnackers(allElvesTotalCalories){
+    const sortedSnacksAscending = allElvesTotalCalories.sort((oneSnack, anotherSnack) => oneSnack - anotherSnack);
+    const bottomThreeSnackers = sortedSnacksAscending.slice(0, 3);
+    return bottomThreeSnackers;
+}
+
+function getTopThreeSnackers(allElvesTotalCalories){
+    const sortedSnacksDescending = allElvesTotalCalories.sort((oneSnack, anotherSnack) => anotherSnack - oneSnack);
+    const topThreeSnackers = sortedSnacksDescending.slice(0, 3);
+    return topThreeSnackers;
+}
+
+function getTotalCaloriesFromArrayOfSnacks(arrayOfSnacks){
+    return arrayOfSnacks.reduce((previous, current) => previous + current);
+}
+
 console.log(mostCalorificSnackage);
+console.log(getBottomThreeSnackers(totalCaloriesForEachElf));
+console.log(getTopThreeSnackers(totalCaloriesForEachElf));
+console.log(getTotalCaloriesFromArrayOfSnacks(getTopThreeSnackers(totalCaloriesForEachElf)));
